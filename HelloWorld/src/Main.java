ee2e405647ba70a99a1a62f4540b07db971b0f13
Haifeng Shen <137964267+haifshen@users.noreply.github.com>
2024-09-04 11:40:55 +1000

    Add files via upload

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String s = "Hello world!";
+        String s = "Hello world Again!";
         System.out.println(s);
     }
 }
